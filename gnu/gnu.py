b2befae29eecaeb2d97cced13d1a5be214aaee56
ritsu <ritsu@users.noreply.github.com>
2016-09-09 05:01:05 -0400

    Add sed

--- gnu/gnu.py
+++ gnu/gnu.py
@@ -18,7 +18,7 @@ class GNU:
     max_message_length = 1900
 
     # using a dict in case command and function are different; key = cmd, value = func
-    command_list = {"grep": "grep", "wc": "wc", "tail": "tail", "cat": "cat", "tac": "tac"}
+    command_list = {"grep": "grep", "wc": "wc", "tail": "tail", "cat": "cat", "tac": "tac", "sed": "sed"}
 
     # used to match url input
     url_pattern = re.compile(
@@ -196,7 +196,7 @@ class GNU:
                         out = line
                     # print output
                     if pipe:
-                        pipe_out.append(out.strip("\n"))
+                        pipe_out.append(out)
                     else:
                         await self.bot.say("```{0}```".format(out))
                         lines_displayed += 1
@@ -457,7 +457,7 @@ class GNU:
                     return
             # display output
             if pipe:
-                pipe_out.append(line.strip("\n"))
+                pipe_out.append(line)
             else:
                 await self.bot.say("```{0}```".format(line))
                 lines_displayed += 1
@@ -596,7 +596,7 @@ class GNU:
                     return
             # display output
             if pipe:
-                pipe_out.append(out.strip("\n"))
+                pipe_out.append(out)
             else:
                 await self.bot.say("```{0}```".format(out))
                 lines_displayed += 1
@@ -741,7 +741,7 @@ class GNU:
                     return
             # display output
             if pipe:
-                pipe_out.append(line.strip("\n"))
+                pipe_out.append(line)
             else:
                 await self.bot.say("```{0}```".format(line))
                 lines_displayed += 1
@@ -766,6 +766,307 @@ class GNU:
             else:
                 await ctx.invoke(func, pipe_in="\n".join(pipe_out))
 
+    @commands.command(pass_context=True, name='sed')
+    async def sed(self, ctx, *args, **kwargs):
+        """A simple stream editor
+
+        Type !sed for more information.
+        """
+
+        if not args:
+            await self.bot.say("*sed* is a simple stream editor")
+            await self.bot.say("```sed [option] [script] [input]```")
+            await self.bot.say("```"
+                               "\nOption"
+                               "\n\t-g      Process entire input as a single string, rather than line by line."
+                               "\n\t-n      Disable automatic printing; only produce output when explicitly told to."
+                               "\n\nScript Address"
+                               "\n\t/.*/    Returns lines that match the regular expression."
+                               "\n\tA       Returns line number A."
+                               "\n\tA,B     Returns lines from A to B."
+                               "\n\tA~N     Returns every Nth line, starting from A"
+                               "\n\nScript Command"
+                               "\n\ta...    Append after each line."
+                               "\n\tc...    Change lines with new line."
+                               "\n\td       Delete lines."
+                               "\n\ti...    Insert before each line."
+                               "\n\tp       Print line."
+                               "\n\ts/././  Substitute with regular expression pattern."
+                               "\n\t=       Print line number."
+                               "\n\nScript Pattern Flag"
+                               "\n\t/I      Ignore case"
+                               "\n\t/p      Print (mostly used when -n option is active)"
+                               "\n\nInput"
+                               "\n\tURL     If input matches a URL pattern, will attempt to fetch URL content."
+                               "\n\t        By default, DOM will be parsed from URL content and text elements will be treated as \"lines\""
+                               "\n\t        If -p option is set, URL content will be treated as plain text."
+                               "\n\t@chat   If '@chat' is specified as the input, will search in chat log."
+                               "\n\t        Logging must be activated in the channel for this to work."
+                               "\n\t<input> If none of the previous inputs are detected, remaining text is treated as input."
+                               "\n\t        To preserve whitespace (including newlines), enclose entire input in quotes."
+                               "```")
+            return
+
+        # parse input
+        option = set()
+        script = ""
+        input = []
+        pipe = []
+        iterator = args.__iter__()
+        for arg in iterator:
+            if arg == '|':
+                while True:
+                    try:
+                        pipe.append(next(iterator))
+                    except:
+                        break
+            elif arg[0] == '-':
+                option.add(arg[1:])
+            elif not script:
+                script = arg
+            else:
+                input.append(arg)
+        input = " ".join(input)
+
+        if not script:
+            await self.bot.say("Command not found.")
+            return
+
+        # try pipe_in if input is empty
+        if not input and "pipe_in" in kwargs:
+            input = kwargs["pipe_in"]
+
+        if not input:
+            await self.bot.say("Input not found.")
+            return
+
+        # get address
+        address_type = ""
+        address = ""
+        if script[0] == '/':
+            address_type = "regex"
+            match = re.compile(r"^/(.*?)/(?<!\\/)i?", re.IGNORECASE).match(script)
+            # get regex for address range
+            if match.group(0)[-1].lower() == 'i':
+                address = re.compile(r"{0}".format(match.group(1)), re.IGNORECASE)
+            else:
+                address = re.compile(r"{0}".format(match.group(1)))
+            # shift script
+            script = script[match.end():]
+        elif re.compile(r"^[$\d]").match(script):
+            match = re.compile(r"^[$\d]+[,~]?[$\d]*").match(script)
+            # get tuple for address range
+            if ',' in match.group(0):
+                address_type = "range"
+                address = (match.group(0)[:match.group(0).index(',')], match.group(0)[match.group(0).index(',') + 1:])
+            elif '~' in match.group(0):
+                address_type = "step"
+                address = (match.group(0)[:match.group(0).index('~')], match.group(0)[match.group(0).index('~') + 1:])
+            else:
+                address_type = "line"
+                address = match.group(0)
+            # shift script
+            script = script[match.end():]
+        else:
+            address_type = "blank"
+            address = "all"
+
+        # get command
+        sed_commands = {'a', 'c', 'd', 'i', 'p', 's', '='}
+        script = script.strip()
+        command = script[0]
+        if command not in sed_commands:
+            await self.bot.say("Unknown command: `{0}`".format(command))
+            return
+
+        if command in ('a', 'c', 'i', 's'):
+            if len(script) < 2:
+                await self.bot.say("Expected characters after: `{0}`".format(command))
+                return
+            acis_line = script[1:]
+        elif command == 'd':
+            if len(script) > 1:
+                await self.bot.say("Extra characters after command: `{0}`".format(command))
+                return
+        elif command == 'p':
+            if len(script) > 1:
+                await self.bot.say("Extra characters after command: `{0}`".format(command))
+                return
+
+        if command == 's':
+            if acis_line[0] != '/' or acis_line.count('/') < 3:
+                await self.bot.say("Unknown substitution pattern: `{0}`".format(acis_line))
+                return
+            sub = re.compile(r"^(.*?)/(?<!\\/)(.*?)/(?<!\\/)(.*)").match(acis_line[1:])
+            if len(sub.groups()) != 3:
+                await self.bot.say("Unknown substitution pattern: `{0}`".format(acis_line))
+                return
+            if sub.groups()[2].lower() not in ("i", "p", ""):
+                await self.bot.say("Unrecognized pattern flag: `{0}`".format(sub.groups()[2]))
+                return
+            sub_flag = sub.groups()[2].lower()
+            sub_replace = sub.groups()[1]
+            try:
+                if sub_flag.lower() == 'i':
+                    sub_search = re.compile(sub.groups()[0], re.IGNORECASE)
+                else:
+                    sub_search = re.compile(sub.groups()[0], re.IGNORECASE)
+            except:
+                await self.bot.say("Error trying to create substitution pattern: `{0}`".format(sub.groups()[0]))
+                return
+        elif command == '=':
+            if len(script) > 1:
+                await self.bot.say("Extra characters after command: `{0}`".format(command))
+                return
+
+        #await self.bot.say("command: " + command)
+
+        if GNU.url_pattern.match(input):
+            #await self.bot.say("Querying `" + input + "`")
+            async with aiohttp.ClientSession() as session:
+                async with session.get(input) as response:
+                    response_text = await response.text()
+                    if 'p' in option:
+                        # plain text url content
+                        if 'g' in option:
+                            input = [response_text]
+                        else:
+                            input = response_text.splitlines()
+                    else:
+                        # parse DOM
+                        def visible(element):
+                            if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+                                return False
+                            elif re.match('<!--.*-->', str(element)):
+                                return False
+                            return True
+                        soup = BeautifulSoup(response_text, "html.parser")
+                        texts = soup.findAll(text=True)
+                        input = filter(visible, texts)
+                        if 'g' in option:
+                            input_string = "\n".join([line for line in input])
+                            input = [input_string]
+        elif input.lower() == "@chat":
+            # handle chat log input
+            await self.bot.say("Sorry, @chat is not supported at this time.")
+            return
+        else:
+            # user input or pipe
+            if 'g' in option:
+                input = [input]
+            else:
+                input = input.splitlines()
+
+        # fix up address
+        if address_type in ("range", "step"):
+            if address[0] == '$':
+                address[0] = len(input)
+            else:
+                address[0] = int(address[0])
+        elif address_type == "line":
+            if address == '$':
+                address = len(input)
+            else:
+                address = int(address)
+        if (address_type == "range") and (address[0] >= address[1]):
+            address_type = "line"
+            address = address[0]
+
+        # do sed
+        pipe_out = []
+        lines_displayed = 0
+        for i, line in enumerate(input):
+            line_num = i + 1
+            # determine if match
+            match = False
+            if address_type == "blank":
+                match = True
+            elif address_type == "range":
+                if line_num >= address[0] and line_num <= address[1]:
+                    match = True
+            elif address_type == "step":
+                if line_num >= address[0] and ((line_num - address[0]) % address[1]) == 0:
+                    match = True
+            elif address_type == "line":
+                if line_num == address:
+                    match = True
+            elif address_type == "regex":
+                if address.search(line):
+                    match = True
+
+            # build out
+            out = []
+            if match:
+                # insert
+                if command == 'i':
+                    out.append(acis_line)
+                # print
+                elif command == 'p':
+                    out.append(line)
+                # print line
+                elif command == '=':
+                    out.append(line_num)
+                # change
+                elif command == 'c':
+                    if address_type == "range" and line_num != address[0]:
+                        line = ''
+                    else:
+                        line = acis_line
+                # substitute
+                elif command == 's':
+                    try:
+                        line = re.sub(sub_search, sub_replace, line)
+                    except:
+                        await self.bot.say("Error duing substitution on line {}".format(line_num))
+
+            # silent option
+            if 'n' in option and not (command == 's' and sub_flag == 'p'):
+                pass
+            # echo line
+            else:
+                out.append(line)
+
+            if match:
+                # append
+                if command == 'a':
+                    out.append(acis_line)
+                # delete
+                elif command == 'd':
+                    out = []
+
+            for s in out:
+                # truncate messages that are too long
+                if len(s) > GNU.max_message_length and not pipe:
+                    s = s[:(GNU.max_message_length - 12)] + " [TRUNCATED]"
+                # flood prevention
+                if lines_displayed > 0 and lines_displayed % GNU.more_limit == 0:
+                    await self.bot.say("Type 'more' or 'm' to continue...")
+                    answer = await self.bot.wait_for_message(timeout=15, author=ctx.message.author)
+                    if not answer or answer.content.lower() not in ["more", "m"]:
+                        await self.bot.say("Command output stopped.")
+                        return
+                # display output
+                if s:
+                    if pipe:
+                        pipe_out.append(s)
+                    else:
+                        await self.bot.say("```{0}```".format(s))
+                        lines_displayed += 1
+
+        # handle pipe
+        if pipe:
+            cmd = pipe[0]
+            if cmd[0] != ctx.prefix:
+                await self.bot.say("```{0}: command prefix missing```".format(cmd))
+                return
+            elif cmd[1:] not in GNU.command_list.keys():
+                await self.bot.say("```{0}: command not found```".format(cmd))
+                return
+            func = getattr(GNU, GNU.command_list[cmd[1:]])
+            if len(pipe) > 1:
+                await ctx.invoke(func, *pipe[1:], pipe_in="\n".join(pipe_out))
+            else:
+                await ctx.invoke(func, pipe_in="\n".join(pipe_out))
 
 
 def setup(bot):
