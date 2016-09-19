82d1171d67b0a3510956bf8b5e18c6ad2c49e4cc
ritsu <ritsu@users.noreply.github.com>
2016-09-19 05:01:45 -0400

    Added overall cpu usage, made output more consistent

--- sysinfo/sysinfo.py
+++ sysinfo/sysinfo.py
@@ -23,50 +23,67 @@ class SysInfo:
         """Show CPU, Memory, Disk, and Network information"""
 
         # CPU
-        cpu_cs = "CPU Count: {0} Physical, {1} Logical".format(psutil.cpu_count(logical=False), psutil.cpu_count())
+        cpu_cs = ("CPU Count"
+                  "\n\t{0:<9}: {1:>2}".format("Physical", psutil.cpu_count(logical=False)) +
+                  "\n\t{0:<9}: {1:>2}".format("Logical", psutil.cpu_count()))
         psutil.cpu_percent(interval=None, percpu=True)
         await asyncio.sleep(1)
-        cpu_ps = "CPU Usage: {0}".format(psutil.cpu_percent(interval=None, percpu=True))
+        cpu_p = psutil.cpu_percent(interval=None, percpu=True)
+        cpu_ps = ("CPU Usage"
+                  "\n\t{0:<8}: {1}".format("Per CPU", cpu_p) +
+                  "\n\t{0:<8}: {1:.1f}".format("Overall", sum(cpu_p)/len(cpu_p)))
         cpu_t = psutil.cpu_times()
-        numlen = len(str(max([cpu_t.user, cpu_t.system, cpu_t.idle])))
+        width = max([len("{:,}".format(int(n))) for n in [cpu_t.user, cpu_t.system, cpu_t.idle]])
         cpu_ts = ("CPU Times"
-                  "\n\t{0:<7}: {1:>{width},}".format("User", int(cpu_t.user), width=numlen) +
-                  "\n\t{0:<7}: {1:>{width},}".format("System", int(cpu_t.system), width=numlen) +
-                  "\n\t{0:<7}: {1:>{width},}".format("Idle", int(cpu_t.idle), width=numlen))
+                  "\n\t{0:<7}: {1:>{width},}".format("User", int(cpu_t.user), width=width) +
+                  "\n\t{0:<7}: {1:>{width},}".format("System", int(cpu_t.system), width=width) +
+                  "\n\t{0:<7}: {1:>{width},}".format("Idle", int(cpu_t.idle), width=width))
 
         # Memory
         mem_v = psutil.virtual_memory()
+        width = max([len(self._size(n)) for n in [mem_v.total, mem_v.free, (mem_v.total - mem_v.free)]])
         mem_vs = ("Virtual Memory"
-                  "\n\t{0:<10}: {1:>6}".format("Total", self._size(mem_v.total)) +
-                  "\n\t{0:<10}: {1:>6}".format("Available", self._size(mem_v.available)) +
-                  "\n\t{0:<10}: {1:>6}".format("Used", str(mem_v.percent) + "%"))
+                  "\n\t{0:<10}: {1:>6}".format("Total", self._size(mem_v.total), width=width) +
+                  "\n\t{0:<10}: {1:>6}".format("Available", self._size(mem_v.available), width=width) +
+                  "\n\t{0:<10}: {1:>{width}} {2}%".format("Used", self._size(mem_v.total - mem_v.free),
+                                                          mem_v.percent, width=width))
         mem_s = psutil.swap_memory()
+        width = max([len(self._size(n)) for n in [mem_s.total, mem_s.free, (mem_s.total - mem_s.free)]])
         mem_ss = ("Swap Memory"
-                  "\n\t{0:<6}: {1:>6}".format("Total", self._size(mem_s.total)) +
-                  "\n\t{0:<6}: {1:>6}".format("Free", self._size(mem_s.free)) +
-                  "\n\t{0:<6}: {1:>6}".format("Used", str(mem_s.percent) + "%"))
+                  "\n\t{0:<6}: {1:>{width}}".format("Total", self._size(mem_s.total), width=width) +
+                  "\n\t{0:<6}: {1:>{width}}".format("Free", self._size(mem_s.free), width=width) +
+                  "\n\t{0:<6}: {1:>{width}} {2}%".format("Used", self._size(mem_s.total - mem_s.free),
+                                                         mem_s.percent, width=width))
 
         # Open files
         open_f = psutil.Process().open_files()
-        common = os.path.commonpath([f.path for f in open_f]) if open_f else ""
-        open_fs = "\n\t".join(["{0} [{1}]".format(f.path.replace(common, '.'), f.mode) for f in open_f])
-        open_fs = "Open File Handles\n\t" + open_fs
+        open_fs = "Open File Handles\n\t"
+        if open_f:
+            common = os.path.commonpath([f.path for f in open_f])
+            open_fs = "\n\t".join(["{0} [{1}]".format(f.path.replace(common, '.'), f.mode) for f in open_f])
+            open_fs += open_fs
+        else:
+            open_fs += "None"
 
         # Disk usage
         disk_u = psutil.disk_usage(os.path.sep)
+        width = max([len(self._size(n)) for n in [disk_u.total, disk_u.free, disk_u.used]])
         disk_us = ("Disk Usage"
-                   "\n\t{0:<6}: {1:>8}".format("Total", self._size(disk_u.total)) +
-                   "\n\t{0:<6}: {1:>8}".format("Free", self._size(disk_u.free)) +
-                   "\n\t{0:<6}: {1:>8} {2}%".format("Used", self._size(disk_u.used), disk_u.percent))
+                   "\n\t{0:<6}: {1:>{width}}".format("Total", self._size(disk_u.total), width=width) +
+                   "\n\t{0:<6}: {1:>{width}}".format("Free", self._size(disk_u.free), width=width) +
+                   "\n\t{0:<6}: {1:>{width}} {2}%".format("Used", self._size(disk_u.used),
+                                                          disk_u.percent, width=width))
 
         # Network
         net_io = psutil.net_io_counters()
+        width = max([len(self._size(n)) for n in [net_io.bytes_sent, net_io.bytes_recv]])
         net_ios = ("Network"
-                   "\n\t{0:<11}: {1:>8}".format("Bytes sent", self._size(net_io.bytes_sent)) +
-                   "\n\t{0:<11}: {1:>8}".format("Bytes recv", self._size(net_io.bytes_recv)))
+                   "\n\t{0:<11}: {1:>{width}}".format("Bytes sent", self._size(net_io.bytes_sent), width=width) +
+                   "\n\t{0:<11}: {1:>{width}}".format("Bytes recv", self._size(net_io.bytes_recv), width=width))
 
         # Boot time
-        boot_s = ("Boot Time: {0}".format(datetime.fromtimestamp(
+        boot_s = ("Boot Time"
+                  "\n\t{0}".format(datetime.fromtimestamp(
                        psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")))
 
         await self.bot.say("```" +
